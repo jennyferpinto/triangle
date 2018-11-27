62aca6d8b5b3d58e64dc7de03458ebd9e7c3c0fb
jennyferpinto <pinto.jenyfer@gmail.com>
2018-11-27 15:20:36 +0100

    fixed Triangle class methods and added Junit tests

--- src/main/java/triangle/Triangle.java
+++ src/main/java/triangle/Triangle.java
@@ -8,12 +8,19 @@ import java.util.*;
 public class Triangle {
 
 	private int[] t = new int[3];
+	private Boolean isScalene = false;
+	private Boolean isEquilateral = false;
+	private Boolean isIsosceles = false;
 
 	public Triangle(int a, int b, int c) {
+		// check to see if dimensions make a legal triangle before calling constructors
 		if (a + b > c && a + c > b && b + c > a) {
 			this.t[0] = a;
 			this.t[1] = b;
 			this.t[2] = c;
+			this.isEquilateral = isEquilateral(this);
+			this.isIsosceles =  isIsosceles(this);
+			this.isScalene = isScalene(this);
 		} else {	
 		throw new java.lang.Error("Not a triangle!");
 		}
@@ -23,48 +30,65 @@ public class Triangle {
 		return t;
 	}
 
-	public boolean isEquilateral() {
-		if (t[0] == t[1] && t[0] == t[2]) {
+	public Boolean getEquilateralProperty() {
+		return isEquilateral;
+	}
+
+	public Boolean getScaleneProperty() {
+		return isScalene;
+	}
+
+	public Boolean getIsoscelesProperty() {
+		return isIsosceles;
+	}
+
+	private Boolean isEquilateral(Triangle triangle) {
+		if (triangle.t[0] == triangle.t[1] && triangle.t[0] == triangle.t[2]) {
+			return true;
+		}
+		return false;
+	}
+	
+	private Boolean isIsosceles(Triangle triangle) {
+		if (triangle.isEquilateral) { // every equilateral triangle is isosceles
+			return true; 
+		} else if (triangle.t[0] == triangle.t[1] || triangle.t[0] == triangle.t[2] || triangle.t[1] == triangle.t[2]) {
 			return true;
 		}
 		return false;
 	}
 
-	// every equilateral triangle is isosceles, but not every isosceles triangle is equilateral. 
-	public boolean isIsosceles() {
-		
-		if (this.isEquilateral()) {
-			return true;
-		} else {
-			if (t[0] == t[1] || t[0] == t[2]) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean isScalene() {
-		if (t[0] != t[1] && t[0] != t[2]) {
+	private Boolean isScalene(Triangle triangle) {
+		if (triangle.isEquilateral) { // if triangle is isoscles then can't be equilateral
+			return false;
+		} else if (triangle.t[0] != triangle.t[1] && triangle.t[0] != triangle.t[2] && triangle.t[1] != triangle.t[2]) {
 			return true;
 		}
 		return false;
 	}
 
-	// public String checkTriangle() {
-	// 	this.isIsosceles();
-	// 	this.isScalene();
+	public String returnFormattedResults() {
+		StringBuilder builder = new StringBuilder();
 
-	// 	if (this.isEquilateral()) {
+		builder.append(String.format("%s = %b, ", "equilateral", this.isEquilateral));
+		builder.append(String.format("%s = %b, ", "isosceles", this.isIsosceles));
+		builder.append(String.format("%s = %b", "scalene", this.isScalene));
 
-	// 	}
-	// }
+		return builder.toString();
+	}
 
 	public static void main(String[] args) {
-		Triangle t = new Triangle(3,4,5);
-		// Triangle t = new Triangle(3,4,4);
-		System.out.println(Arrays.toString(t.getDimensions()));
-		System.out.println("Is it equilateral? " + t.isEquilateral());
-		System.out.println("Is it scalene? " + t.isScalene());
-		System.out.println("Is it isosceles? " + t.isIsosceles());
+		Triangle t1 = new Triangle(4,4,4);
+		Triangle t2 = new Triangle(3,4,4);
+		Triangle t3 = new Triangle(2,4,4);
+		
+		System.out.println(Arrays.toString(t1.getDimensions()));
+		System.out.println(t1.returnFormattedResults());
+
+		System.out.println(Arrays.toString(t2.getDimensions()));
+		System.out.println(t2.returnFormattedResults());
+
+		System.out.println(Arrays.toString(t3.getDimensions()));
+		System.out.println(t3.returnFormattedResults());
 	}
 }
--- src/test/java/triangle/Tests.java
+++ src/test/java/triangle/Tests.java
@@ -1,13 +1,34 @@
 package main.java.triangle;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
 public class Tests {
 
     @Test
-    public void isScaleneReturnsTrue() {
-        Triangle t = new Triangle(3,4,5); 
-        // assert statements
-        assertEquals(true, t.isScalene(), "should return true");
-        assertEquals(false, t.isEquilateral(), "should return false");
-        assertEquals(false, t.isIsosceles(), "should return false");
+    public void scaleneIsTrue() {
+        Triangle t = new Triangle(3,4,5);
+        assertEquals(true, t.getScaleneProperty(),  "should return true");
+        assertEquals(false, t.getEquilateralProperty(),"should return false");
+        assertEquals(false, t.getIsoscelesProperty(), "should return false");
+    }
+
+    @Test
+    public void equilateralAndIsoscelesisTrue() {
+        Triangle t = new Triangle(5,5,5); 
+
+        assertEquals(true, t.getEquilateralProperty(), "should return true");
+        assertEquals(true, t.getIsoscelesProperty(), "should return true");
+        assertEquals(false, t.getScaleneProperty(), "should return false");
+    }
+
+    @Test
+    public void isoscelesIsTrue() {
+        Triangle t = new Triangle(3,4,4); 
+
+        assertEquals(true, t.getIsoscelesProperty(), "should return true");
+        assertEquals(false, t.getEquilateralProperty(), "should return false");
+        assertEquals(false, t.getScaleneProperty(), "should return false");
     }
 }
